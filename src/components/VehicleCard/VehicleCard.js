a79989e8274463d17ba4ff2324f9373ef521cafa
unknown <sayooj0076@gmail.com>
2023-08-09 14:06:50 +0530

    2:06

--- src/components/VehicleCard/VehicleCard.js
+++ src/components/VehicleCard/VehicleCard.js
@@ -1,19 +1,116 @@
 // MUI //
-import { Box } from "@mui/material";
+import { Box, Rating, Divider, Button,styled } from "@mui/material";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+// CSS //
+import classes from "./Vehicle.module.css";
+// Custom //
+const RegisterButton = styled(Button)({
+  color:'white',
+  backgroundColor: "#ff4d30",
+  fontWeight: 400,
+  fontSize: "1.2rem",
+  width: "80%",
+  maxWidth: "388px",
+  marginBottom:'10px',
+  "&:hover": {
+    backgroundColor: "#f34826",
+  },
+  "&:hover svg": {
+    color: "#00e676",
+  },
+});
 
-const VehicleCard = () => {
+const VehicleCard = (props) => {
   return (
     <Box>
-      <Box>
-        <img />
+      <Box
+        sx={{
+          width: "95%",
+          border: "1px solid grey",
+          boxShadow: "1px 2px 5px 5px rgba(0,0,0,0.75)",
+        }}
+      >
+        <Box>
+          <img src={props.imagePath} className={classes.imageTag} />
+        </Box>
+        <Box className={classes.DetailsContainer}>
+          <Box className={classes.SubContainer}>
+            <Box
+              sx={{
+                width: "50%",
+                paddingLeft: "4px",
+                fontWeight: "600",
+                fontSize: "2rem",
+              }}
+            >
+              {props.model}
+            </Box>
+            <Box
+              sx={{
+                width: "50%",
+                textAlign: "right",
+                fontWeight: "600",
+                fontSize: "2rem",
+              }}
+            >
+              ${props.rent}
+            </Box>
+          </Box>
+        </Box>
+        <Box className={classes.DetailsContainer}>
+          <Box className={classes.SubContainer}>
+            <Box sx={{ width: "50%" }}>
+              <Rating name="read-only" value={4} readOnly />
+            </Box>
+            <Box sx={{ width: "50%", textAlign: "right" }}>per day</Box>
+          </Box>
+        </Box>
+        <Box className={classes.DetailsContainer}>
+          <Box className={classes.SubContainer}>
+            <Box sx={{ width: "50%", display: "flex", alignItems: "center" }}>
+              <DirectionsCarIcon sx={{ marginRight: "5px" }} />
+              <span>{props.model}</span>
+            </Box>
+            <Box
+              sx={{
+                width: "50%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+              }}
+            >
+              <span>{props.doors}</span>
+              <DirectionsCarIcon sx={{ marginLeft: "5px" }} />
+            </Box>
+          </Box>
+        </Box>
+        <Box className={classes.DetailsContainer}>
+          <Box className={classes.SubContainer}>
+            <Box sx={{ width: "50%", display: "flex", alignItems: "center" }}>
+              <DirectionsCarIcon sx={{ marginRight: "5px" }} />
+              <span>{props.transmission}</span>
+            </Box>
+            <Box
+              sx={{
+                width: "50%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+              }}
+            >
+              <span>{props.fuel}</span>
+              <DirectionsCarIcon sx={{ marginLeft: "5px" }} />
+            </Box>
+          </Box>
+        </Box>
+        <Divider
+          variant="middle"
+          sx={{ borderBottomWidth: "4px", margin: "10px 10px" }}
+        />
+        <Box sx={{display:'flex',justifyContent:'center'}}>
+          <RegisterButton>Book Ride</RegisterButton>
+        </Box>
       </Box>
-      <Box sx={{display:'flex',justifyContent:'space-between'}}>
-        <h2>Audi A1</h2>
-        <h2>$50</h2>
-      </Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
     </Box>
   );
 };
--- src/components/pages/VehicleModals/VehicleModals.js
+++ src/components/pages/VehicleModals/VehicleModals.js
@@ -1,15 +1,30 @@
 // MUI //
-import { Box } from "@mui/material";
+import { Box,Grid } from "@mui/material";
 // Custom //
 import VehicleCard from "../../VehicleCard/VehicleCard";
 import CustomNavBar from "../../CustomNavBar/CustomNavBar";
+// Data //
+import CarData from "../../../Data/CarData";
 
 const VehicleModals = () => {
   return (
     <Box>
       <CustomNavBar />
-      <Box>
-        <VehicleCard />
+      <Box sx={{padding:'2rem 2rem'}}>
+        <Grid container spacing={3}>
+        {CarData.map((item) => (
+          <Grid item xs={12}>
+          <VehicleCard
+            model={item.model}
+            doors={item.doors}
+            transmission={item.transmission}
+            fuel={item.fuel}
+            imagePath={item.imagePath}
+            rent={item.rent}
+          />
+          </Grid>
+        ))}
+        </Grid>
       </Box>
     </Box>
   );
